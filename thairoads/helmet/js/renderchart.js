e97765b92bb69639fafacb39d0fdd5fc2f82375f
Rito <ruangrit@opendream.co.th>
2017-06-12 16:12:43 +0700

    filter year

--- thairoads/helmet/js/renderchart.js
+++ thairoads/helmet/js/renderchart.js
@@ -6,63 +6,80 @@ $(document).ready(function () {
 
     var initYearStart = 2554;
     var initYearEnd = 2558;
+    var yearStart;
+    var yearEnd;
 
-    var yearStart = initYearStart;
-    var yearEnd = initYearEnd;
-    var catId = '';
-    var categories;
-    var series = [];
+    submitFilter();
 
-    var codeDetail = [
-       {'d1': 'dddd'},
-       {'d2': 'dddd2'},
-    ];
+    function submitFilter() {
 
-    var codeValueList = $('.code-value-filter-hidden').serialize();
-    var codeNameList = $('.code-name-filter-hidden').serialize();
-    $.ajax({
-      type: 'POST',
-      url: Drupal.settings.basePath + 'helmet/chartdata',
-      dataType: 'json',
-      success: function(data1) {
+        if ($('#start_year').val() == null) {
+           yearStart = initYearStart;
+           yearEnd = initYearEnd;
+        } 
+        else {
+
+           yearStart = $('#start_year').val(); 
+           yearEnd = $('#end_year').val(); 
+
+        }
+
+        var catId = '';
+        var categories;
+        var series = [];
+
+        var codeDetail = [
+           {'d1': 'dddd'},
+           {'d2': 'dddd2'},
+        ];
+
+        var codeValueList = $('.code-value-filter-hidden').serialize();
+        var codeNameList = $('.code-name-filter-hidden').serialize();
+        $.ajax({
+          type: 'POST',
+          url: Drupal.settings.basePath + 'helmet/chartdata',
+          dataType: 'json',
+          success: function(data1) {
 
 
-        var year_list = "";
-        $.each(data1['year_list'], function(key_y, value_y) {
-          year_list += "<option value="+value_y+" class="+value_y+">"+value_y+"</option>";
-        }); 
+            var year_list = "";
+            $.each(data1['year_list'], function(key_y, value_y) {
+              year_list += "<option value="+value_y+" class="+value_y+">"+value_y+"</option>";
+            }); 
 
-        $('#start_year').html(year_list);
-        $('#end_year').html(year_list);
+            $('#start_year').html(year_list);
+            $('#end_year').html(year_list);
 
-        $('#start_year  .'+yearStart).attr({'selected': 'selected'});
-        $('#end_year  .'+yearEnd).attr({'selected': 'selected'});
+            $('#start_year  .'+yearStart).attr({'selected': 'selected'});
+            $('#end_year  .'+yearEnd).attr({'selected': 'selected'});
 
 
-        var runIndex = 0;  
-        $.each(data1['data'], function(index1, value1) {
-          // do your stuff here
-            var tempData = [];
-            var tempYear = [];
-            $.each(value1, function(index2, value2) {
-                tempData.push(value2); 
-                tempYear.push(index2); 
+            var runIndex = 0;  
+            $.each(data1['data'], function(index1, value1) {
+              // do your stuff here
+                var tempData = [];
+                var tempYear = [];
+                $.each(value1, function(index2, value2) {
+                    tempData.push(value2); 
+                    tempYear.push(index2); 
+                });
+
+                series[runIndex] = {
+                    name: index1,
+                    data: tempData, 
+                }
+
+                categories = tempYear;
+
+              runIndex++;
             });
 
-            series[runIndex] = {
-                name: index1,
-                data: tempData, 
-            }
-
-            categories = tempYear;
-
-          runIndex++;
+            renderChart(categories, series, 'อัตราการสวมหมวกนิรภัยตั้งแต่ปี ' + yearStart + ' ถึง ' + yearEnd);
+          },
+          data: 'yearStart='+yearStart+'&catId='+catId+'&yearEnd='+yearEnd+'&'+codeValueList+'&'+codeNameList
         });
 
-        renderChart(categories, series);
-      },
-      data: 'yearStart='+yearStart+'&catId='+catId+'&yearEnd='+yearEnd+'&'+codeValueList+'&'+codeNameList
-    });
+    }
 
     function renderChart(categories, series, title) {
 
@@ -71,7 +88,7 @@ $(document).ready(function () {
                 type: 'line'
             },
             title: {
-                text: 'Monthly Average Temperature'
+                text: title 
             },
             xAxis: {
                 categories: categories 
@@ -79,12 +96,16 @@ $(document).ready(function () {
             yAxis: {
                 title: {
                     text: ''
+                },
+                labels: {
+                    format: "{value} %"
                 }
             },
             plotOptions: {
                 line: {
                     dataLabels: {
-                        enabled: true
+                        enabled: true,
+                        format: '{y} %'
                     },
                     enableMouseTracking: false
                 }
@@ -94,5 +115,9 @@ $(document).ready(function () {
         });
 
     }
+
+    $('#filter_btn').click(function () {
+        submitFilter();
+    });
 
 });
